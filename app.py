f2a8f3d9bcd80fc800319db9d62eec45afa1fd68
Kamariddin Ibodullaev <51526189+Kamariddin0505@users.noreply.github.com>
2025-12-09 12:44:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
+import requests
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+API_KEY = "YOUR_API_KEY"  # ⚠️ Подставь ключ OpenWeatherMap
+CITY = "Tashkent"
+
+
+@app.route("/")
+def temperature():
+    url = (
+        "https://api.openweathermap.org/data/2.5/weather"
+        f"?q={CITY}&appid={API_KEY}&units=metric"
+    )
+    response = requests.get(url).json()
+    temp = response["main"]["temp"]
+    return jsonify({
+        "city": CITY,
+        "temperature_c": temp
+    })
+
+
+@app.route("/health")
+def health():
+    return "OK", 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
 
